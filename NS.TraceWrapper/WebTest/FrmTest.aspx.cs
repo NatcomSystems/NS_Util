84e50de0b8177ab1f4e3a5a07aacd68f0e0ee0de
accatamayo <accatamayo@natcomsystems.net>
2014-02-17 15:19:28 -0500

    Primer Commit
    - Agregado el utilitario y proyecto de prueba

---
+++ NS.TraceWrapper/WebTest/FrmTest.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NS.TraceWrapper;
+
+namespace WebTest
+{
+    public partial class FrmTest : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new Exception("Excepcion Test");
+            }
+            catch (Exception ex)
+            {
+                ExceptionTrace.Write(ex);
+                // Incluir manejo de excepcion
+            }
+        }
+    }
+}
